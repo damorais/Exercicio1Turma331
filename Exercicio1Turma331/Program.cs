898fd7c9adf224dab138b4272b7454314aed03cc
Daniel Morais <damorais@gmail.com>
2020-03-10 17:10:15 -0300

    Criação do projeto

---
+++ Exercicio1Turma331/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio1Turma331
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
